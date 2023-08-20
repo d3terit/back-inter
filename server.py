69d95fe4ca753860fd32f8ff7f07b3b6ff40d210
Daniel Mantilla <42301040+d3terit@users.noreply.github.com>
2023-08-19 23:52:25 -0500

    Update server.py
    Signed-off-by: Daniel Mantilla <42301040+d3terit@users.noreply.github.com>

--- server.py
+++ server.py
@@ -8,10 +8,17 @@ port = int(os.environ.get("PORT", 5000))
 @app.route('/static/<path:path>')
 def serve_static(path):
     return send_from_directory('static', path)
+listSing = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o']
+exclude = ['el','la','los','les']
 
-@app.route('/')
+@app.route("/")
 def home():
-   return render_template('index.html')
+    sings = ['h','o','l','a']
+    #retorna un objeto json
+    return {
+        "msg": "hola",
+        "sings": sings
+    }
 
 @app.route('/<path:path>')
 def all_routes(path):
